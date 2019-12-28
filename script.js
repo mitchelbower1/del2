3be5d6024a7e6f6b5798724392631a1961875aa8
Mitchel Bower <mitchelbower@outlook.com>
2019-12-28 01:20:58 -0500

    Add comments

--- script.js
+++ script.js
@@ -1,49 +1,62 @@
+// Variable declarations
 let vacationType = "";
 let groupSize;
 let destination;
 let travelSuggestion;
+
+// While loop to make sure user in putting in desired input
 while (
-  vacationType.toLowerCase() !== "musical" &&
-  vacationType.toLowerCase() !== "tropical" &&
-  vacationType.toLowerCase() !== "adventurous"
+    vacationType.toLowerCase() !== "musical" &&
+    vacationType.toLowerCase() !== "tropical" &&
+    vacationType.toLowerCase() !== "adventurous"
 ) {
-  vacationType = prompt(
-    "What kind of trip would you like to go on? (Musical, tropical, or adventurous)"
-  );
+    vacationType = prompt(
+        "What kind of trip would you like to go on? (Musical, tropical, or adventurous)"
+    );
 }
+
+// While loop to make sure user is putting in a number
 while (isNaN(parseInt(groupSize))) {
     groupSize = prompt('How many people are you going with?');
 
 }
 
+// Parsing group size to int to eliminate redundancy
 groupSize = parseInt(groupSize);
 
-switch (vacationType.toLowerCase()) {
-    case 'musical':
-        destination = 'New Orleans.';
-        break;
-    
-    case 'Tropical':
-        destination = 'beach vacation in Mexico.';
-        break;
-    
-    case 'adventurous':
-        destination = 'whitewater rafting in the Grand Canyon.'
-        break;
+// Lower casing vacationType to eliminate redundancy
+vacationType = vacationType.toLowerCase();
+
+// Vacation type conditional to determine destination
+if (vacationType === 'musical') {
+
+    destination = 'New Orleans';
+}
+else if (vacationType === 'tropical') {
+
+    destination = 'beach vacation in Mexico.';
+}
+else if (vacationType === 'adventurous') {
+
+    destination = 'whitewater rafting in the Grand Canyon';
 }
 
+
+// Group size conditional to determine travel suggestion
 if (groupSize <= 2) {
     travelSuggestion = 'first class flight';
 }
 
-if (groupSize >= 3 && groupSize <= 5) {
+else if (groupSize >= 3 && groupSize <= 5) {
     travelSuggestion = 'helicopter'
 }
 
-if (groupSize > 5) {
+else if (groupSize > 5) {
     travelSuggestion = 'charter flight'
 }
 
-alert(`Since you are a group of ${groupSize} going on a ${vacationType} vacation, youshould take a ${travelSuggestion} to ${destination} (Also printed to console)`)
+// Alerting results to window
+alert(`Since you are a group of ${groupSize} going on a ${vacationType} vacation, you should take a ${travelSuggestion} to ${destination} (Also printed to console)`)
 
-console.log(`Since you are a group of ${groupSize} going on a ${vacationType} vacation, yous hould take a ${travelSuggestion} to ${destination}`)
+// Logging results to console
+console.log(`Since you are a group of ${groupSize} going on a ${vacationType} vacation, you should take a ${travelSuggestion} to ${destination}`)
